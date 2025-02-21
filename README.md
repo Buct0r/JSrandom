01a48985ab66680315f28a95a3af9986f73bd514
Buct0r <pietroburani08@gmail.com>
2025-02-21 22:49:02 +0100

    Update 2.0 for JSrandom

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Introducing 
 # **JSrandom**
-![JSrandom logo](JSrandomlogo.png)
+![JSrandom logo](webapp/JSrandomlogo.png)
 ## A random password generator made with Javascript
-JSrandom is a secure random password generator, built with HTML, CSS and most importantly in Javascript.
+JSrandom is a secure random password generator, built with HTML, CSS and most importantly with Javascript.
 
 Your password will be generated in the matter of seconds after choosing the parameters you want for your password. 
 * **Lenght**
@@ -36,7 +36,7 @@ Just use the web version you can find at this [link](https://buct0r.github.io/JS
 
 #### Windows
 Go to the [release page](https://github.com/Buct0r/JSrandom/releases), download the executable and run it. As simple as that.
-You can also run the executable in the terminal, also because is a terminal application
+You can also run the executable in the terminal, also because is a terminal application.
 
 
 #### Linux
@@ -68,10 +68,17 @@ and then:
 ```
 ./JSrandom.py
 ```
+# Update 2.0
 
+This is the biggest update of JSrandom, with a new introduction page, new effects for the UI and the introduction of a copy button (finally) on the webapp, to quickly copy your passwords.
+
+## Browser extensions
+I'm also introducing the web extension of JSrandom for chromium based browsers and Firefox. For Firefox just broswe to the AMO page at this [link](https://addons.mozilla.org/it/firefox/addon/jsrandom/). For the chromium version, for now I can't publish it on the Chrome web store, but you can find it on the [release page](https://github.com/Buct0r/JSrandom/releases) as a zip file, that you can add to your browser enabling developer mode and uploading the zip file.
 
 ### Conclusion
 Since the executable is not signed, your antivirus will probably block it. If you want you can add an exeption in your antivirus settings or if you don't trust the exe, just download the python file and run it with python. 
+
+In the next week I will try to find a solution to add the extension to the Chrome web store.
 
 Thanks to skesko for the collaboration with the testing phase. :3
 
---
+++ cli/JSrandom.py
@@ -0,0 +1,74 @@
+import random
+
+print("***************JSrandom***************")
+while True:
+    lettersL = "abcdefghijklmnopqrstuvwxyz"
+    lettersU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "1234567890"
+    symbols = "!@#$%^&*()_+="
+    allowedValues = ""
+    generatedPassword = ""
+
+
+    print("**************************************")
+    print("[1] Generate Password")
+    print("[2] Exit")
+
+    try:
+        choice = int(input())
+    except ValueError:
+        choice = 0
+    if(choice == 1):
+        try:
+            length = int(input("Enter the lenght: "))
+        except ValueError:
+            length = 0
+        if length <= 0:
+            print("Password length must be at least one character long\n")
+        else:
+            includeLettersL = input("Do you want to include lowercase letters? [Y/N] (default: y): ")
+            if(includeLettersL == "Y" or includeLettersL == "y" or includeLettersL == ""):
+                allowedValues += lettersL
+                LetL = True
+            else:
+                LetL = False
+                allowedValues += ""
+            includeLettersU = input("Do you want to include uppercase letters? [Y/N] (default: y): ")
+            if(includeLettersU == "Y" or includeLettersU == "y" or includeLettersU == ""):
+                allowedValues += lettersU
+                LetU = True
+            else:
+                LetU = False
+                allowedValues += ""
+            includeNums = input("Do you want to include numbers? [Y/N] (default: y): ")
+            if(includeNums == "Y" or includeNums== "y" or includeNums == ""):
+                allowedValues += numbers
+                Num = True
+            else:
+                Num = False
+                allowedValues += ""
+            includeSyms = input("Do you want to include symbols? [Y/N] (default: y): ")
+            if(includeSyms == "Y" or includeSyms == "y" or includeSyms == ""):
+                allowedValues += symbols
+                Syms = True
+            else:
+                Syms = False
+                allowedValues += ""
+            if LetL == False and LetU == False and Num == False and Syms == False:
+                print("At least one parameter has to be selected\n")
+            else:
+                for i in range(0, length):
+                    randomIndex = random.randint(0, len(allowedValues))
+                    try:
+                        generatedPassword += allowedValues[randomIndex]
+                    except IndexError:
+                        randomIndex = random.randint(0, len(allowedValues))
+                        generatedPassword += allowedValues[randomIndex]
+                print("Generated password:\n", generatedPassword)
+
+    elif(choice == 2):
+        break
+    else:
+        print("Command not found :/")
+     
+print("**************************************")
